9c928f72860920d3e774804b49574bb5b7041a5e
Tejas JM <93418229+Shisui-12@users.noreply.github.com>
2023-02-13 11:59:04 +0530

    Update 215_Kth_Largest_Element.cpp

--- Medium/215_Kth_Largest_Element.cpp
+++ Medium/215_Kth_Largest_Element.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end(),greater <>());
-        for( auto i : nums)
-        {
-            cout << i << ' ';
-        }
+//         for( auto i : nums)
+//         {
+//             cout << i << ' ';
+//         }
         int ans = nums.at(k - 1);
 
         return ans;
