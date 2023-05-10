d045eb7a6bafcb8062d8995b23049713ff6616c7
Tejas JM <93418229+Shisui-12@users.noreply.github.com>
2023-05-10 09:11:03 +0530

    Update 88_Merge_Sorted_Array.cpp
    Faster than 100.0% Solutions

--- Easy/88_Merge_Sorted_Array.cpp
+++ Easy/88_Merge_Sorted_Array.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int len = nums1.size();
-        int zero_index = m;
-        int j = 0;
-        for(int i = zero_index; i < len; i++)
+        for(int i = m, j = 0; i < len; i++, j++)
         {
           nums1.at(i) = nums2.at(j);
-          j++;
         }
         sort(nums1.begin(), nums1.end());
     }
