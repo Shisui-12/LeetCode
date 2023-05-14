9b1495c5a8c6cfc7a2dcb731678d6ee68b46f18a
Tejas JM <93418229+Shisui-12@users.noreply.github.com>
2023-05-14 11:50:44 +0530

    Day 10 Challenge

---
+++ JS_30_Day_Challenge/2632_Curry.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var curry = function(fn) {
+    return function curried(...args) {
+        if(args.length >= fn.length)
+            {
+                return fn(...args)
+            }
+        else
+            {
+                return function(...args2)
+                {
+                    return curried(...args, ...args2)
+                }
+            }
+    };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
